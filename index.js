c1d18ac5d7ebd99e0189533d8a3b41df8694a66e
LaurentiuMolnar <lau.molnar2013@gmail.com>
2017-10-18 16:50:56 +0300

    Added master layout and .env

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
+// Load environment variables
+require('dotenv').config();
+
+// Require dependencies
 const express = require('express');
 const app = express();
 const path = require('path');
+const mysql = require('mysql');
+const hbs = require('express-handlebars');
 
+// Connect to the database
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
 
-app.get('/', (req, res) => res.render('./index.html'));
+connection.connect((err) => {
+  if(err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected as id ' + connection.threadId);
+});
 
-app.use(express.static(path.join(__dirname, 'app')));
-app.listen(3000, (port) => console.log("Listening on port " + port));
+// Configure the app
+app.set('views', path.join(__dirname, 'app/views'));
+app.engine('html', hbs({
+  defaultLayout: 'master',
+  extname: '.html',
+  layoutsDir: 'app/views/layouts/',
+  partialsDir: 'app/views/partials'
+}));
+app.set('view engine', 'html');
+
+app.get('/', (req, res) => res.render('index.html'));
+
+// Define path for static files
+app.use('/', express.static('app'));
+
+// Start the app
+app.listen(process.env.PORT, process.env.HOST, () => console.log("Listening on port " + process.env.PORT + ", " + new Date()));
